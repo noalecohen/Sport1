33dbfdebbda802b56133691e42c16afe089363d6
Noa Cohen <noac@moveo.co.il>
2022-02-28 12:06:44 +0200

    changed more screen text

--- src/screens/MoreScreen.tsx
+++ src/screens/MoreScreen.tsx
@@ -4,7 +4,7 @@ import {View, Text, StyleSheet} from 'react-native';
 const MoreScreen = () => {
   return (
     <View style={styles.text}>
-      <Text>More Screen</Text>
+      <Text>More Screen Test</Text>
     </View>
   );
 };
